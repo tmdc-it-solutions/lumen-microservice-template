0c8618d4a07350c171429abdfee6ef3e6c3561d8
jg-rivera <rivera.jga10@gmail.com>
2021-07-13 09:05:44 +0800

    fix: remove RabbitMQ queue connection service provider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -96,7 +96,6 @@ $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
 $app->register(App\Providers\RabbitEventsServiceProvider::class);
-$app->register(VladimirYuldashev\LaravelQueueRabbitMQ\LaravelQueueRabbitMQServiceProvider::class);
 $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 $app->register(Spatie\EventSourcing\EventSourcingServiceProvider::class);
 
